ca0116fd9b71294d98b0034214d37cc191a51df3
Yuri Tagomori <goncalvessyuri@gmail.com>
2023-05-23 15:51:55 -0300

    adding shortcut and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   # Defines the root path route ("/")
   # root "articles#index"
 
   # List of tasks
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
 
-  # POST request to submit the form
-  post 'tasks', to: 'tasks#create'
+  # # POST request to submit the form
+  # post 'tasks', to: 'tasks#create'
 
-  # Display the task form
-  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # # Display the task form
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
 
-  # Upate a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # # Upate a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 
-  # Show details of a task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Show details of a task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # Receive the edit form
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Receive the edit form
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete a task
+  # # Delete a task
   delete 'tasks/:id', to: 'tasks#destroy'
 end
