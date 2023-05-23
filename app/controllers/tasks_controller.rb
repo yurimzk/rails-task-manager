f59f44c9f9b52a33dfe73ef7350f85a91ed94e5e
Yuri Tagomori <goncalvessyuri@gmail.com>
2023-05-23 15:30:03 -0300

    rails task manager done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # List all tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # Show details of a task
+  def show
+  end
+
+  # Instance used to build the form
+  def new
+    @task = Task.new
+  end
+
+  # POST request to submit the form and create a task
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path
+  end
+
+  # Instace used to build the form
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List of tasks
+  get 'tasks', to: 'tasks#index'
+
+  # POST request to submit the form
+  post 'tasks', to: 'tasks#create'
+
+  # Display the task form
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # Upate a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # Show details of a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Receive the edit form
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
